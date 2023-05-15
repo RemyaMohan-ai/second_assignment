a63d0e0c3f6dafb8959c638e6c2600271a5d117e
RemyaMohan-ai <remya04mohan@gmai.com>
2023-05-15 20:36:39 +0530

    gradding system

---
+++ asqn3.js
@@ -0,0 +1,13 @@
+let mark=52;
+if(mark>90){
+    console.log("you scored A grade");
+} else if(mark>70){
+    console.log("you scored B grade");
+}else if(mark>50){
+    console.log("you scored C grade");
+}else if(mark>30){
+    console.log("you scored D grade")
+}else{
+    console.log("you are failed");
+}
+    
